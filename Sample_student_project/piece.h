3320fc1db6a31fb1c6a36093a44d0618bd60aa71
John Thomas Marion Wolfe <Johnwolfe@Supermac100000.local>
2018-10-02 18:10:28 -0400

    Added doxygen documentation to the piece file.

--- Sample_student_project/piece.h
+++ Sample_student_project/piece.h
@@ -3,6 +3,10 @@
 // November 23, 2015
 // Project7
 
+/** @file piece.h
+* @brief A class that represents a piece on a board
+* @author John Wolfe
+*/
 #ifndef PIECE_H
 #define PIECE_H
 enum color {black, white, blank};
@@ -11,6 +15,9 @@ class piece {
 public:
 	piece() {theColor = blank;}
 
+/** 
+* @brief Switches the colors of the piece. Does nothing if there's no piece
+*/
 	void flip()
 	{
 		if (theColor == white) {
@@ -20,7 +27,10 @@ public:
 			theColor = white;
 		}
 	}
-
+/**
+* @brief Checks if the piece is currently blank
+* @return Returns a bool. 1 if blank, 0 if a piece is there.
+*/
 	bool is_blank()const {return theColor == blank;}
 	bool is_black()const {return theColor == black;}
 	bool is_white()const {return theColor == white;}
